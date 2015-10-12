26429690bc297e73b3651a373ee8eae04c432e32
1423073 <1423073@stud.hs-mannheim.de>
2015-10-12 12:40:13 +0200

    Klassen erstellt, Konstruktoren, Get-Methode, Attribute

---
+++ exercise2/src/FlaecheAuslesen/Figur.java
@@ -0,0 +1,21 @@
+package FlaecheAuslesen;
+
+public class Figur {
+
+	//Attribute
+	private int flaeche = 0;
+	
+	//Constructor
+	public Figur () {
+		
+	}
+	
+	
+	//Get Methode
+	public int getFlaeche() {
+		return flaeche;
+	
+	}	
+}
+	
+	
---
+++ exercise2/src/FlaecheAuslesen/Rechteck.java
@@ -0,0 +1,13 @@
+package FlaecheAuslesen;
+
+public class Rechteck extends Figur {
+	
+	//Attribute
+	int höhe = 5;
+	int breite = 10;
+
+	//Constructor 
+	public Rechteck (int höhe, int breite){
+		super();	
+	}	
+}
