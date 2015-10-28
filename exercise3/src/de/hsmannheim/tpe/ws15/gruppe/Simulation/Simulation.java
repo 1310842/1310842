608e16787329e9d98d4cf9d9d12fb7aa69e62256
kuebra <kuebra@10.10.123.92>
2015-10-28 11:40:41 +0100

    hhh

---
+++ exercise3/src/de/hsmannheim/tpe/ws15/gruppe/Simulation/Simulation.java
@@ -0,0 +1,17 @@
+package de.hsmannheim.tpe.ws15.gruppe.Simulation;
+
+public class Simulation {
+	
+	/** Method start game
+	 */
+	protected void runGame (Simulation start) {
+		
+	}
+	
+	/** Constructor
+	 */
+	public Simulation () {
+		
+	}
+
+}
---
+++ exercise3/src/de/hsmannheim/tpe/ws15/gruppe11/Gamecontroller/Gamecontroller.java
@@ -0,0 +1,33 @@
+package de.hsmannheim.tpe.ws15.gruppe11.Gamecontroller;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Gamecontroller {
+	
+	/** Attribute
+	 */
+	protected int runde;
+	protected int spieler1;
+	protected int spieler2;
+	
+	/** Method information about player and round
+	 */
+	public int aktuelleRundeSpieler () 
+	}
+	
+	/** Constructor
+	 */
+	public Gamecontroller () {
+		
+	}
+	
+	/** Start game
+	 */
+	public void runGame(Gamecontroller start) {
+		
+	
+		
+		
+	}
+}
