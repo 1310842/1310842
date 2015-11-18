abe495a1c565283b2f7ee93f5cdba61bbca6872c
Car, Isra <1310842@stud.hs-mannheim.de>
2015-11-18 11:28:20 +0100

    JavaDoc der Klasse Buerger überarbeitet.

--- exercise4/src/de/hsmannheim/tpe/ws15/mensch/Buerger.java
+++ exercise4/src/de/hsmannheim/tpe/ws15/mensch/Buerger.java
@@ -4,18 +4,48 @@ import de.hsmannheim.tpe.ws15.staat.Finanzamt;
 import de.hsmannheim.tpe.ws15.steuer.EinkommenssteuerI;
 import de.hsmannheim.tpe.ws15.steuer.Einkommensteuer;
 
+/**
+ * Die Klasse Buerger ist eine Unterklasse von Mensch. Sie Präsentiert die
+ * Eigenschaften eines Buergers, die  in Metropolis leben.
+ * 
+ * @author Car, Isra.
+ * @author Celik, Kuebra.
+ */
+
 public class Buerger extends Mensch implements EinkommenssteuerI {
+
+	/**
+	 * Instanziierung des Buergers. Die Parameter definieren die Anfangswerte
+	 * für die Klasse Buerger.
+	 * 
+	 * @param name
+	 *            Der Name des Buerger.
+	 * @param einkommen
+	 *            Das Einkommen des Buerger.
+	 * @param alter
+	 *            Der Alter des Buerger.
+	 */
 
 	public Buerger(String name, int einkommen, int alter) {
 		super(name, einkommen, alter);
 		Finanzamt.anmelden(this);
 	}
 
+	/**
+	 * Die Methode berechneEinkommenssteuer berechnet vom Buerger die
+	 * Einkommenssteuer und gibt diese zurück.
+	 */
+
 	@Override
 	public int berechneEinkommenssteuer() {
 		return (Einkommensteuer.berechneEinkommensSteuer(this.einkommen));
 
 	}
+
+	/**
+	 * Die Methode toString gibt die Information Name und Alter über den Buerger
+	 * aus.
+	 */
 
 	@Override
 	public String toString() {
