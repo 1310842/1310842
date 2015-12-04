9c24f7d30698c24c4d7d25c6a8fd6d268228b445
i.car <i.car@141.19.145.39>
2015-12-04 14:28:04 +0100

    Test angepasst

--- exercise5/src/de/hsmannheim/tpe/ws15/gruppe11/simulation/JUnit.java
+++ exercise5/src/de/hsmannheim/tpe/ws15/gruppe11/simulation/JUnit.java
@@ -3,10 +3,10 @@ package de.hsmannheim.tpe.ws15.gruppe11.simulation;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import de.hsmannheim.tpe.ws15.gruppe11.bundesnachrichtendienst.Crypter;
-import de.hsmannheim.tpe.ws15.gruppe11.bundesnachrichtendienst.CrypterFactory;
 import de.hsmannheim.tpe.ws15.gruppe11.bundesnachrichtendienst.IllegalKeyException;
 import de.hsmannheim.tpe.ws15.gruppe11.bundesnachrichtendienst.IllegalMessageException;
 import de.hsmannheim.tpe.ws15.gruppe11.enumeration.CrypterVerfahren;
+import de.hsmannheim.tpe.ws15.gruppe11.verschluesselung.CrypterFactory;
 
 /**
  * Die Klasse JUnit ueberprueft das Programm.
