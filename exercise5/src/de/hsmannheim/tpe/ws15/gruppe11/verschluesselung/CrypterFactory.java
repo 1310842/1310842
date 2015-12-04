cab5378c0a39a260c92cef0fb3b0d7fab12701f1
i.car <i.car@141.19.145.39>
2015-12-04 14:29:37 +0100

    Final

---
+++ exercise5/src/de/hsmannheim/tpe/ws15/gruppe11/verschluesselung/CrypterFactory.java
@@ -0,0 +1,40 @@
+package de.hsmannheim.tpe.ws15.gruppe11.verschluesselung;
+
+import de.hsmannheim.tpe.ws15.gruppe11.bundesnachrichtendienst.Crypter;
+import de.hsmannheim.tpe.ws15.gruppe11.enumeration.CrypterVerfahren;
+
+/**
+ * Die Klasse CrypterFactory stellt durch die Methode <b>createCrypter</b>
+ * Implementierungsmoeglichkeiten zur Verschluesselung dar.
+ * 
+ * @author Car, Isra
+ * @author Celik, Kuebra
+ *
+ */
+
+public class CrypterFactory {
+
+	public CrypterFactory() {
+
+	}
+
+	public static Crypter createCrypter(final CrypterVerfahren crypterVerfahren) {
+		switch (crypterVerfahren)
+
+		{
+
+		case CAESAR:
+			return new CrypterCaesar();
+		case NULL:
+			return new CrypterNull();
+		case SUBSTITUTION:
+			return new CrypterSubstitution();
+		case XOR:
+			return new CrypterXOR();
+		default:
+			throw new IllegalArgumentException("Existiert  nicht.");
+		}
+
+	}
+
+}
