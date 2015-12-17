55760aa0c383ff130d2fa79fd29fd81a11ea2fd3
kuebra <kuebra@10.10.123.92>
2015-12-17 16:47:23 +0100

    Javadoc Parameter entfernt

--- exercise6/src/de/hsmannheim/tpe/ws15/gruppe11/impl/NodeListImpl.java
+++ exercise6/src/de/hsmannheim/tpe/ws15/gruppe11/impl/NodeListImpl.java
@@ -11,7 +11,6 @@ import de.hsmannheim.tpe.ws15.gruppe11.interfaces.NodeList;
  * @author Car, Isra
  * @author Celik, Kübra
  * 
- * @param <T>
  */
 
 public class NodeListImpl<T> extends ListImpl<Node<T>> implements NodeList<T> {
