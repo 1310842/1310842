4e94394022fbd6df540736edcc85a820e4bad5bb
kuebra <kuebra@10.10.123.92>
2015-12-17 18:18:51 +0100

    Suchverfahren erstellt

---
+++ exercise6/src/de/hsmannheim/tpe/ws15/gruppe11/search/Suchverfahren.java
@@ -0,0 +1,15 @@
+package de.hsmannheim.tpe.ws15.gruppe11.search;
+
+/**
+ * Die Suchmethoden werden dargestellt die dann zur Auswahl bei einer
+ * Suchmethodenerstellung dienen
+ * 
+ * @author Car, Isra
+ * @author Celik, Kübra
+ */
+
+public enum Suchverfahren {
+
+	BREITENSUCHE, TIEFENSUCHE
+
+}
