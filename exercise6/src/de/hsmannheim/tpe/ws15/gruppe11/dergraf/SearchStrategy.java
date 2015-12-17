e19c6ff55ce0246fb4d29475890e53605cc0694e
kuebra <kuebra@10.10.123.92>
2015-12-17 14:24:10 +0100

    fehlerbehebung

--- exercise6/src/de/hsmannheim/tpe/ws15/gruppe11/dergraf/SearchStrategy.java
+++ exercise6/src/de/hsmannheim/tpe/ws15/gruppe11/dergraf/SearchStrategy.java
@@ -2,9 +2,7 @@ package de.hsmannheim.tpe.ws15.gruppe11.dergraf;
 
 public interface SearchStrategy<T> {
 
-	public static void search(Node<T> rootNode, T value) {
-
-	}
+	public NodeList<T> search(Node<T> node, T value);
 
 	public NodeList<T> getPath();
 
