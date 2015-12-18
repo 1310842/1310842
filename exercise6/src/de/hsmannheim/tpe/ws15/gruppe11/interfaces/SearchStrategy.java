3e5a53cdc379ae5577d22fd59d1262433f64a0fd
kuebra <kuebra@10.10.123.92>
2015-12-18 20:59:45 +0100

    JavaDoc ergänzt.

--- exercise6/src/de/hsmannheim/tpe/ws15/gruppe11/interfaces/SearchStrategy.java
+++ exercise6/src/de/hsmannheim/tpe/ws15/gruppe11/interfaces/SearchStrategy.java
@@ -10,9 +10,23 @@ import de.hsmannheim.tpe.ws15.gruppe11.dergraf.Node;
  */
 
 public interface SearchStrategy<T> {
-
+	/**
+	 * Methode mir der nach Knoten mit einem bestimmten Wert gesucht werden
+	 * kann.
+	 * 
+	 * @param node
+	 *            Knoten, von dem die Suche gestartet werden soll
+	 * @param value
+	 *            Wert nach dem gesucht werden soll
+	 * @return gefundene Knoten als Datentyp NodeList
+	 */
 	public NodeList<T> search(Node<T> node, T value);
 
+	/**
+	 * Gibt den Suchpfad der letzten durchgeführten Suche zurückgibt.
+	 * 
+	 * @return Pfad von der letzten Suche als Datentyp NodeList
+	 */
 	public NodeList<T> getPath();
 
 }
